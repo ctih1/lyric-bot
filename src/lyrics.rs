0fb42c92f191405cb3c0822450e70eb14c79e268
Onni Nevala <nevalaonni@gmail.com>
2025-01-23 17:46:49 +0200

    checkpoint!

--- src/lyrics.rs
+++ src/lyrics.rs
@@ -1,8 +1,51 @@
-use scraper::Html;
-use ureq;
 
-pub fn get_lyrics(artist_name: &str, song_name: &str) -> str {
-    ureq::get(format!("https://genius.com/{}-{}-lyrics",artist_name,song_name))
-    .header("User-Agent","Mozilla/5.0 (Linux; Android 12; DE2118) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36");
+use slugify::slugify;
+use scraper::Html;
+use scraper::Selector;
+use reqwest;
+
+
+async fn get_lyrics_data(artist_name: &str, song_name: &str) -> String {
+    let html: Result<String, reqwest::Error> = reqwest::get(
+        format!("https://genius.com/{}-{}-lyrics",slugify!(artist_name),slugify!(song_name))
+    )
+        .await
+        .unwrap()
+        .text()
+        .await;
+    println!("Getting {}",format!("https://genius.com/{}-{}-lyrics",slugify!(artist_name),slugify!(song_name)));
+    return html.unwrap();
 }
+
+
+fn html_to_discord_formatter(html: &mut String) {
+    *html = html
+        .replace("<i>","*")
+        .replace("</i>","*")
+        .replace("<b>","**")
+        .replace("</b>","**")
+        .replace("<br>","\n")
+        .replace("&amp","&")
+}
+
+fn process_lyrics(html: &str) -> String {
+    let document: Html = Html::parse_document(html);
+    let selector = Selector::parse(r#"[data-lyrics-container="true"] > a > span, [data-lyrics-container="true"] > * "#).unwrap();
+
+    let mut html:String = String::new();
+
+    for lyrics_part in document.select(&selector) {
+        html += &lyrics_part.text().collect::<Vec<_>>()[0];
+        html += "\n"; // This was the easiest way I could think of 
+    }
+    html_to_discord_formatter(&mut html);
+    return html;
+}
+
+pub async fn get_lyrics(artist_name: &str, song_name: &str) -> String {
+    return process_lyrics(get_genius_page(artist_name, song_name).await.as_str());
+}
+
+
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod lyrics;
-
-use core::time;
+#[macro_use]
+extern crate slugify;
 use std::env;
+use std::collections::HashMap;
 
 use dotenv::dotenv;
 
-use poise::serenity_prelude as serenity;
+use poise::{serenity_prelude as serenity, CreateReply};
 use poise::serenity_prelude::*;
 
 // use serenity::all::standard::CommandResult;
@@ -16,11 +17,48 @@ struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-struct Handler;
-
 #[poise::command(slash_command, prefix_command)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("Pong!").await?;
+    Ok(())
+}
+
+#[poise::command(slash_command, prefix_command)]
+async fn lyrics(
+    ctx:Context<'_>,
+    #[description="User whose song to pick"] user: Option<serenity::User>
+) -> Result<(), Error> {
+    let activities: HashMap<UserId, Presence> = ctx.guild().as_ref().unwrap().presences.clone();
+    let activity_data: Option<&Vec<Activity>> = activities.get(&ctx.author().id).map(|presence | &presence.activities);
+    let mut song_name:String = String::new();
+    let mut artist_name:String = String::new(); 
+
+    if let Some(activities) = activity_data {
+        for activity in activities {
+            if activity.name.to_lowercase() != "spotify" { continue; }
+            
+            song_name = activity.details.as_ref().unwrap_or(&String::from("Unknown")).clone();
+            artist_name = activity.state.as_ref().unwrap_or(&String::from("Unknown")).clone();
+
+            break;
+        }
+    } else {
+        println!("No activities!");
+    }
+
+
+    let lyrics:String = lyrics::get_lyrics(&artist_name, &song_name).await;
+
+    println!("{}",lyrics);
+
+    let embed = CreateEmbed::new()
+        .title(format!("Lyrics for {} by {}", song_name, artist_name))
+        .description(lyrics);
+
+
+    let response = poise::CreateReply::default().embed(embed);
+
+    ctx.send(response).await?;
     Ok(())
 }
 
@@ -29,15 +67,13 @@ async fn ping(ctx: Context<'_>) -> Result<(), Error> {
 async fn main() {
     dotenv().ok();
     let token = env::var("BOT_TOKEN").expect("Expected a token in the environment");
-    println!("{token}");
+    println!("{token}");    
 
-    lyrics::get_lyrics("kanye-west", "ultralight-beam");
-
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let intents: GatewayIntents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILDS | GatewayIntents::GUILD_MEMBERS | GatewayIntents::GUILD_PRESENCES;
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![ping()],
+            commands: vec![ping(), lyrics()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
@@ -46,7 +82,7 @@ async fn main() {
                 Ok(Data {})
             })
         })
-        .build();
+        .build(); 
 
 
     let client = ClientBuilder::new(token,intents)
